b2e4b67a0fa7150b61b635333987c15a06460e68
gtsircou <1722710@mukilteo.wednet.edu>
2025-01-29 10:19:57 -0800

    spent an hour getting a basic transparency script working chat am i cooked

---
+++ project manila/Assets/Scripts/TRANSparency.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRANSparency : MonoBehaviour //i cant take it anymore
+{
+    public KeyCode click = KeyCode.Mouse0, rClick = KeyCode.Mouse1;
+    public SpriteRenderer SpriteRenderer;
+    public bool isTrans = false; //alright buddy
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(click))
+        {
+            Debug.Log("that did something!");
+        }
+        if (Input.GetKeyDown(rClick))
+        {
+            if (isTrans == false)
+            {
+                enable(SpriteRenderer);
+                isTrans = true;
+            }
+            else
+            {
+                disable(SpriteRenderer);
+                isTrans = false;
+            }
+        }
+    }
+    private static void enable(SpriteRenderer SpriteRenderer)
+    {
+        SpriteRenderer.color = new Color(1, 1, 1, 0.25f);
+        Debug.Log("enabled");
+    }
+    private static void disable(SpriteRenderer SpriteRenderer)
+    {
+        SpriteRenderer.color = new Color(1, 1, 1, 1f);
+        Debug.Log("disabled");
+    }
+}
