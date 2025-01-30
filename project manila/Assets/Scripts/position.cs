4a49e417ecd839ea96c12bd82754c60c26ffa520
gtsircou <1722710@mukilteo.wednet.edu>
2025-01-30 09:58:18 -0800

    tbh im not sure

---
+++ project manila/Assets/Scripts/position.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject GameObject;
+    public bool isFront;
+    public bool isBack;
+    public bool isCenter;
+    public GameObject vanObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    SortingLayer.GetLayerValueFromName(pos);
+
+    }
+}
